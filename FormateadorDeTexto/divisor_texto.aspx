4bcd825c66e41e8ad446970713a53e4eb3248771
dmunnoz <david.munnozmiranda@estudiante.uam.es>
2016-03-28 14:59:38 +0200

    introduce la silaba aunque no quepa y añadido numeros.

--- FormateadorDeTexto/divisor_texto.aspx
+++ FormateadorDeTexto/divisor_texto.aspx
@@ -1,4 +1,4 @@
-﻿<%@ Page Title="Separador" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="FormateadorDeTexto.WebForm1" %>
+﻿<%@ Page Title="Separador" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="divisor_texto.aspx.cs" Inherits="FormateadorDeTexto.WebForm1" %>
 <asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
 
     <br />
@@ -9,7 +9,7 @@
         <asp:Label ID="LabelIntroduce" Text="Introduce un numero" runat="server"></asp:Label>
         <asp:TextBox ID="Cols" runat="server"></asp:TextBox>
         <asp:RegularExpressionValidator ID="RegularExpressionValidator1" ControlToValidate="Cols" runat="server" ErrorMessage="Sólo puedes introducir un número" ValidationExpression="\d+"></asp:RegularExpressionValidator>
-        <asp:Button ID="Button1" runat="server" OnClick="Formatear" Text="Display Text" />
+        <asp:Button ID="Button1" runat="server" OnClick="Formatear" Text="Formatea la cadena" />
         <br/>
         <asp:TextBox id="TextoAFormatear" TextMode="multiline" Columns="50" Rows="5" runat="server" />
         <asp:TextBox id="TextoFormateado" TextMode="multiline" Columns="50" Rows="5" runat="server" Visible="false" />
--- FormateadorDeTexto/divisor_texto.aspx.cs
+++ FormateadorDeTexto/divisor_texto.aspx.cs
@@ -67,7 +67,7 @@ namespace FormateadorDeTexto
 
             RL.Add('r');
             RL.Add('l');
-            
+            /*
             Prefijos.Add("des");
             Prefijos.Add("pos");
             Prefijos.Add("post");
@@ -85,17 +85,25 @@ namespace FormateadorDeTexto
             Prefijos.Add("tri");
             Prefijos.Add("sin");
             Prefijos.Add("trans");
-
+            */
             Puntuacion.Add(';');
             Puntuacion.Add(',');
             Puntuacion.Add('.');
             Puntuacion.Add(':');
+            Puntuacion.Add('?');
+            Puntuacion.Add('¿');
+            Puntuacion.Add('!');
+            Puntuacion.Add('¡');
+            Puntuacion.Add('"');
+            Puntuacion.Add('(');
+            Puntuacion.Add(')');
 
         }
 
         protected void Formatear(object sender, EventArgs e)
         {
-            String Cadena = "@";            
+            //String Cadena = "@"; 
+            String Cadena = "";           
             String CadenaParseada = "";
             MatchCollection matches;
             Regex regex;
@@ -105,7 +113,7 @@ namespace FormateadorDeTexto
             int cols = int.Parse(Cols.Text);
             TextoFormateado.Columns = cols + 5;
             TextoFormateado.Visible = true;
-            CadenaParseada = parseo(Cadena, CadenaParseada, cols, 0);
+            CadenaParseada = parseo(Cadena, CadenaParseada, cols , 0);
             if (CadenaParseada == null)
             {
                 TextoFormateado.Text = "La cadena no se puede dividir con esas columnas";
@@ -116,25 +124,31 @@ namespace FormateadorDeTexto
             regex = new Regex(@"(\n)");
             matches = regex.Matches(CadenaParseada);
             TextoFormateado.Rows = matches.Count + 1;
+            CadenaParseada = CadenaParseada.Replace("\n\n","\n");
+            CadenaParseada = CadenaParseada.Replace("\n-\n", "\n");
             TextoFormateado.Text = CadenaParseada;
 
         }
         protected string parseo(String CadenaOrigen, String CadenaParseada, int cols, int tamanioAct)
         {
+            if (CadenaOrigen.Length>0 && CadenaOrigen[0] == 32 && tamanioAct==0)
+            {
+                CadenaOrigen = CadenaOrigen.Remove(0, 1);
+            }
             //A la palabra le sobra espacio en la fila
             if (((CadenaOrigen.IndexOf(" ") + tamanioAct) < (cols - 1)) && CadenaOrigen.Length > 0)
             {
+                /*
                 if (CadenaOrigen[0] == '@')
                 {
                     CadenaOrigen = CadenaOrigen.Remove(0, 1);
                 }
-
+                */
                 if (CadenaOrigen.IndexOf(" ") == -1)
                 {
                     CadenaParseada += CadenaOrigen.Substring(0, CadenaOrigen.Length);
                     tamanioAct += CadenaOrigen.Length;
                     CadenaOrigen = CadenaOrigen.Remove(0, CadenaOrigen.Length);
-
                 }
                 else
                 {
@@ -149,10 +163,11 @@ namespace FormateadorDeTexto
             //A la palabra le falta una columna para entrar justo
             else if ((CadenaOrigen.IndexOf(" ") + tamanioAct) == (cols - 1) && CadenaOrigen.Length > 0)
             {
+                /*
                 if (CadenaOrigen[0] == '@')
                 {
                     CadenaOrigen = CadenaOrigen.Remove(0, 1);
-                }
+                }*/
                 if (CadenaOrigen.IndexOf(" ") == -1)
                 {
                     CadenaParseada += CadenaOrigen.Substring(0, CadenaOrigen.Length);
@@ -169,11 +184,11 @@ namespace FormateadorDeTexto
                 return parseo(CadenaOrigen, CadenaParseada, cols, tamanioAct);
             }
             else if ((CadenaOrigen.IndexOf(" ") + tamanioAct == (cols) && CadenaOrigen.Length > 0))
-            {
+            {   /*
                 if (CadenaOrigen[0] == '@')
                 {
                  CadenaOrigen=  CadenaOrigen.Remove(0, 1);
-                }
+                }*/
 
                 CadenaParseada += CadenaOrigen.Substring(0, CadenaOrigen.IndexOf(" "));
                 tamanioAct += CadenaOrigen.IndexOf(" ");
@@ -187,6 +202,7 @@ namespace FormateadorDeTexto
             else if ((CadenaOrigen.IndexOf(" ") + tamanioAct) > (cols - 1) && CadenaOrigen.Length > 0)
             {
                 String silaba;
+                /*
                 if (CadenaOrigen[0] == '@')
                 {
                     silaba = siguiente_silaba(CadenaOrigen.Substring(0, CadenaOrigen.IndexOf(" ") + 1));
@@ -200,20 +216,55 @@ namespace FormateadorDeTexto
                     silaba = siguiente_silaba("@" + CadenaOrigen.Substring(0, CadenaOrigen.IndexOf(" ") + 1));
                 }
                 else
+                */
                     silaba = siguiente_silaba(CadenaOrigen.Substring(0, CadenaOrigen.IndexOf(" ")+1));
+
+                /*
                 if (silaba.Length >= cols)
                     //if(!(CadenaOrigen[silaba.Length]==32))
                         return null;
-
-                if (silaba.Length + 1 + tamanioAct > cols)
+                */
+                if (silaba.Length == 0)
                 {
-                    if (CadenaParseada[CadenaParseada.Length - 1] != 32)
+                    CadenaOrigen = CadenaOrigen.Remove(0, 1);
+                    CadenaParseada += "\n";
+                    tamanioAct = 0;
+                }
+                else if(silaba.Length + tamanioAct > cols)
+                {
+                    if (CadenaParseada.Length > 0 && CadenaParseada[CadenaParseada.Length - 1] != 32)
+                    {
+                        CadenaParseada += "-";
+
+                    }
+                    CadenaParseada += "\n";
+                    tamanioAct = 0;
+                   
+
+                    CadenaParseada += CadenaOrigen.Substring(0, silaba.Length);
+                    tamanioAct += CadenaOrigen.Substring(0, silaba.Length).Length;
+                    CadenaOrigen = CadenaOrigen.Remove(0, silaba.Length);
+                    
+                }/*
+                else if (silaba.Length + tamanioAct > cols)
+                {
+                    if (CadenaParseada.Length != 0  && tamanioAct>1 && CadenaParseada[CadenaParseada.Length - 1] != 32)
                     {
                         CadenaParseada += "-";
                     }
                     CadenaParseada += "\n";
                     tamanioAct = 0;
-                }
+                    CadenaParseada += CadenaOrigen.Substring(0, silaba.Length);
+                    CadenaOrigen = CadenaOrigen.Remove(0, silaba.Length);
+                    if (CadenaParseada.Length!=0 && (CadenaOrigen[0] != 32))
+                    {
+                        CadenaParseada += "-";
+                    }
+
+                    CadenaParseada += "\n";
+                    tamanioAct = 0;
+
+                }*//*
                 else if (silaba.Length + 1 + tamanioAct == cols)
                 {
                     CadenaParseada += CadenaOrigen.Substring(0, silaba.Length);
@@ -221,7 +272,7 @@ namespace FormateadorDeTexto
                     CadenaParseada += "\n";
                     tamanioAct = 0;
                     CadenaOrigen = CadenaOrigen.Remove(0, silaba.Length);
-                }
+                }*/
                 else
                 {
                     CadenaParseada += CadenaOrigen.Substring(0, silaba.Length);
@@ -239,12 +290,20 @@ namespace FormateadorDeTexto
             String silaba="";
             int letras = 1;
             int i;
+            int vocales=0;
             palabra = palabra.Replace("á", "a");
             palabra = palabra.Replace("é", "e");
             palabra = palabra.Replace("í", "i");
             palabra = palabra.Replace("ó", "o");
             palabra = palabra.Replace("u", "u");
+
+            if (Regex.IsMatch(palabra.Substring(0, palabra.IndexOf(" ") + 1), @"\d"))
+            {
+                return palabra.Substring(0,palabra.Length-1);
+            }
+
             //Compruebo si es una palabra nueva para busacar si tiene prefijo
+            /*
             if (palabra[0] == '@')
             {
                 palabra=palabra.Remove(0, 1);
@@ -276,6 +335,23 @@ namespace FormateadorDeTexto
                     palabra.Remove(0, letras);
                     return silaba;
                 }
+            }
+            */
+            for (i = 0; i < palabra.Length; i++)
+            {
+                if (Vocales.Contains(palabra[i]))
+                {
+                    vocales++;
+                }
+            }
+            if (vocales == 0)
+            {
+                return palabra.Substring(0, palabra.Length - 1);
+            }
+
+            if (Puntuacion.Contains(palabra[0]))
+            {
+                silaba += palabra.Substring(0, 1);
             }
             
             //Localizo la primera vocal de la silaba
